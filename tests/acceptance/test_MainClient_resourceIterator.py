ea95eaff8e9a0dc9d90e0883e815e3106e98acef
Robert Metcalf <rmetcalf9@googlemail.com>
2020-06-25 12:43:51 +0100

    Adding iterator test

--- tests/acceptance/test_MainClient_resourceIterator.py
+++ tests/acceptance/test_MainClient_resourceIterator.py
@@ -11,4 +11,46 @@ class helpers(TestHelperSuperClass.testClassWithHelpers):
 
 @TestHelperSuperClass.wipd
 class test_MainClient_resourceIterator(helpers):
-  pass
+  def test_singlePage(self):
+    personGUID="personGUID"
+
+
+    singlePersonResponseDict, _, _ = TestingHelper.getPersonMockResult(personGUID=personGUID, version="6")
+    data=None
+    mockResponse=[singlePersonResponseDict, singlePersonResponseDict, singlePersonResponseDict, singlePersonResponseDict, singlePersonResponseDict]
+
+    self.ethosClient.mock.registerNextResponse(
+      reqFnName="get",
+      url="/api/persons?limit=9&offset=5",
+      data=data,
+      status_code=200,
+      contentBytes=base64.b64encode(json.dumps([]).encode()),
+      contentHeaders={
+        "x-hedtech-media-type": "application/vnd.hedtech.integration.v6+json"
+      },
+      ignoreData=False
+    )
+    self.ethosClient.mock.registerNextResponse(
+      reqFnName="get",
+      url="/api/persons?limit=9&offset=0",
+      data=data,
+      status_code=200,
+      contentBytes=base64.b64encode(json.dumps(mockResponse).encode()),
+      contentHeaders={
+        "x-hedtech-media-type": "application/vnd.hedtech.integration.v6+json"
+      },
+      ignoreData=False
+    )
+
+    personIterator = self.ethosClient.getResourceIterator(
+      loginSession=None,
+      resourceName="persons",
+      version=None,
+      pageSize=9
+    )
+    cur = 0
+    for curPerson in personIterator:
+      cur += 1
+
+    self.assertEqual(cur,5)
+
