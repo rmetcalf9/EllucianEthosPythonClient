a8dd44390b60c8e5bf3b5fae98feb25bd673492f
Robert Metcalf <rmetcalf9@googlemail.com>
2021-01-18 15:35:25 +0000

    Improving error information when response is missing a header

--- tests/acceptance/test_MainClient_resourceIterator.py
+++ tests/acceptance/test_MainClient_resourceIterator.py
@@ -54,3 +54,47 @@ class test_MainClient_resourceIterator(helpers):
 
     self.assertEqual(cur,5)
 
+  # Test commented out as it recreates an issue that needs to be confirmed
+  # def test_responseMissingVersion(self):
+  #   personGUID="personGUID"
+  #
+  #   singlePersonResponseDict, _, _ = TestingHelper.getPersonMockResult(personGUID=personGUID, version="6")
+  #   data=None
+  #   mockResponse=[singlePersonResponseDict, singlePersonResponseDict, singlePersonResponseDict, singlePersonResponseDict, singlePersonResponseDict]
+  #
+  #   self.ethosClient.mock.registerNextResponse(
+  #     reqFnName="get",
+  #     url="/api/persons?limit=9&offset=5",
+  #     data=data,
+  #     status_code=200,
+  #     contentBytes=base64.b64encode(json.dumps([]).encode()),
+  #     contentHeaders=None,
+  #     # contentHeaders={
+  #     #   "x-hedtech-media-type": "application/vnd.hedtech.integration.v6+json"
+  #     # },
+  #     ignoreData=False
+  #   )
+  #   self.ethosClient.mock.registerNextResponse(
+  #     reqFnName="get",
+  #     url="/api/persons?limit=9&offset=0",
+  #     data=data,
+  #     status_code=200,
+  #     contentBytes=base64.b64encode(json.dumps(mockResponse).encode()),
+  #     contentHeaders=None,
+  #     # contentHeaders={
+  #     #   "x-hedtech-media-type": "application/vnd.hedtech.integration.v6+json"
+  #     # },
+  #     ignoreData=False
+  #   )
+  #
+  #   personIterator = self.ethosClient.getResourceIterator(
+  #     loginSession=None,
+  #     resourceName="persons",
+  #     version=None,
+  #     pageSize=9
+  #   )
+  #   cur = 0
+  #   for curPerson in personIterator:
+  #     cur += 1
+  #
+  #   self.assertEqual(cur,5)
