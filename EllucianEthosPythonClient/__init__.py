ddb3821b247ef2283d68573f13dc8b650462f542
Robert <rmetcalf9@googlemail.com>
2024-08-14 10:52:33 +0100

    Update versioneer (using 0.29 now) to address #4

--- EllucianEthosPythonClient/__init__.py
+++ EllucianEthosPythonClient/__init__.py
@@ -1,9 +1,8 @@
 # EllucianEthosPythonClient
 
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 from .EllucianEthosAPIClient import EllucianEthosAPIClient, CanNotStartChangeNotificationPollerTwiceException
 
